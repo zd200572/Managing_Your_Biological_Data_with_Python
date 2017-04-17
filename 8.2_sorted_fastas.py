e9bda9907568b6bd2625f3f828e36265cc8f1e4e
zd200572 <zd200572@yahoo.com>
2017-04-17 20:10:21 +0800

    8.2

--- 8.2_sorted_fastas.py
+++ 8.2_sorted_fastas.py
@@ -1,13 +1,14 @@
+from operator import itemgetter
 table = []
 key = ['name', 'seq', 'length']
 seq = ''
 length = 0
 i = 0
-l = 0
+flag = 0
 inf = open('Homo.fasta', 'r')
 for line in inf:
 	#print(i)
-	if line.startswith('>'):
+	if line.startswith('>') and flag == 1:
 		col = []
 		col.append(line.strip())
 		col.append(seq)
@@ -19,5 +20,14 @@ for line in inf:
 	else:
 		length += len(line.strip())
 		seq += line.strip()
+		flag = 1
 for row in table:
 	print(row[2], '\n')
+table_sorted = sorted(table, key = itemgetter(2))
+out_put = open('out_put.txt', 'w')
+for row in table:
+	i +=1
+	if i <= 3:
+		row = [str(x) for x in row]
+		out_put.write('\t'.join(row) + '\n')
+out_put.close()
