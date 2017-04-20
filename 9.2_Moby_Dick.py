595f55b36af5bee65fb665588ac77798c118cd9f
zd200572 <zd200572@yahoo.com>
2017-04-20 22:12:28 +0800

    i

--- 9.2_Moby_Dick.py
+++ 9.2_Moby_Dick.py
@@ -1,10 +1,36 @@
 import re
 inf = open('Moby_Dick.txt', 'r')
-dict = {}
+dicts = {}
 max = 0
 words = set()
+article = []
+i = 0
 for line in inf:
-	regex = re.compile('[a-zA-Z]+')
-	results = regex.search(line)
-	words.add(results.split('\'')[1])
-	print(results)
+	regexp = re.compile('[a-zA-Z]+')
+	match = regexp.findall(line)
+	if match:
+		for word in match:
+			words.add(word)
+			article.append(word)
+
+#print(words)
+#print(article)
+for a in article:
+	#regex = re.compile('[Ww][Hh][Aa][Ll][Ee]')
+	regex = re.compile('[Cc][Aa][Pp][Tt][Aa][Ii][Nn]')
+	matchs = regex.match(a)
+	if matchs:
+		#print(matchs)
+		#print(matchs.group())
+		i +=1
+print(i)
+	
+for word in words:
+	dicts[word] = 0
+	for x in article:
+		regexp = re.compile('[a-zA-Z]+')
+		match = regexp.findall(x)
+		if match == word:
+			dicts[word] += 1
+
+print(dicts)
